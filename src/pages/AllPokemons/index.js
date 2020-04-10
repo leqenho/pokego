65880c7f0a9bff2beab1a28b79f257bb52c256e8
Alex Freitas <alex@backlog.com.br>
2020-04-10 05:42:48 -0400

    Retornando os primeiros pokemons da PokeApi

--- src/pages/AllPokemons/index.js
+++ src/pages/AllPokemons/index.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import api from '../../services/api';
 
 import './styles.scss';
 
 export default function AllPokemons() {
-  return (
-    <>
-        
-    </>
-  );
+	const [pokemons, setPokemons] = useState([]);
+
+	useEffect(() => {
+		api.get('pokemon?limit=10')
+			.then(({ data }) => {
+				setPokemons(data.results);
+			})
+			.catch(err => {
+				alert(`Falha ao reconhecer pokemons. ${err}`);
+			})
+	}, []);
+
+	return (
+		<>
+			<ul>
+				{pokemons.map(pokemon => (
+					<li>
+						<a href="#">
+							{pokemon.name}
+						</a>
+					</li>
+				))}
+			</ul>
+		</>
+	);
 }
