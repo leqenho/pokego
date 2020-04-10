6b979cdc71971e8c55b84caf77d14c27ad7a787b
Alex Freitas <alex@backlog.com.br>
2020-04-10 05:35:04 -0400

    Configurando estilos e comportamentos

--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import logo from '../../assets/image/';
+import { IoMdMenu } from 'react-icons/io/';
 import "./styles.scss";
 
+import { OpenContext } from '../../App';
+
 export default function Header() {
+    const [open, setOpen] = useContext(OpenContext);
     return (
         <>
-            <header id="Header" >
+            <header id="Header" onClick={() => setOpen(!open)} >
+                <div className="btn__container">
+                    <button className="btn__aside">
+                        <IoMdMenu size={35} />
+                    </button>
+                </div>
+
                 <img className="logo" src={logo} alt="Pokelogo" />
+                <div></div>
             </header>
             <div className="clear"></div>
         </>
